96fcad832fbdb886cf87cb5ad4514cfc08c1bb3a
anaibol <hey@anib.al>
2016-03-05 00:09:06 +0100

    Extends es2015-minimal

--- index.js
+++ index.js
@@ -2,17 +2,8 @@ var modify = require('modify-babel-preset');
 
 var LOOSE = { loose: true };
 
-module.exports = modify('es2015', {
-	// configure relevant plugins into loose mode:
-	'transform-es2015-template-literals': LOOSE,
-	'transform-es2015-classes': LOOSE,
-	'transform-es2015-computed-properties': LOOSE,
-	'transform-es2015-for-of': LOOSE,
-	'transform-es2015-spread': LOOSE,
-	'transform-es2015-destructuring': LOOSE,
-	'transform-es2015-modules-commonjs': LOOSE,
-
-	// remove strict plugins:
-	'transform-es2015-function-name': false,
-	'transform-es2015-typeof-symbol': false,
+module.exports = modify('es2015-minimal', {
+	'transform-es2015-block-scoped-functions': false,
+	'transform-es2015-for-of': false,
+	'check-es2015-constants': false
 });
